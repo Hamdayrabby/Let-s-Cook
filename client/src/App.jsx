347fca11be3a01c0e805b9cfceb195f7cd173ff6
Hamdayrabby <68457391+Hamdayrabby@users.noreply.github.com>
2025-12-28 01:22:12 +0600

    fix: add missing Navigate import to App.jsx
    - Import Navigate from react-router-dom
    - Fixes ReferenceError: Navigate is not defined
    - Resolves blank page issue on deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import home from "./pages/home";
 import savedRecipes from "./pages/savedRecipes";
 import createRecipe from "./pages/createRecipe";
